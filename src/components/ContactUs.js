acef91b864e2f984e0605255aecb9e323595a486
ckm <cuiming1216@gmail.com>
2020-02-13 13:59:28 -0500

    connect to server

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 export default class ContactUs extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {name: '', emai:'', message:''};
+
+    this.handleChangeName = this.handleChangeName.bind(this);
+    this.handleChangeMessage = this.handleChangeMessage.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+  handleChangeMessage(event) {
+    this.setState({message: event.target.value});
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // alert('A name was submitted: ' + this.state.name + this.state.email + this.state.message);
+    // const {name, email, message} = this.state;
+    // const nameemail = {name, email, message};
+
+    // alert(nameemail.name);
+    // axios.post('http://localhost:8080/result_page', nameemail).then(()=>console.log('sucess')).catch(err=>{console.error(err);});
+
+    axios({
+      method: 'POST',
+      url:'https://portfserver.herokuapp.com/',      
+      data:{
+        name: this.state.name,
+        email: this.state.email,
+        message: this.state.message
+      }
+    })
+      .then(function(response){console.log(response);})
+      .catch(function(error){});
+    
+  }
+
   render() {
-    // let resumeData = this.props.resumeData;
+    let resumeData = this.props.resumeData;
     return (
-      // <section id="contact">
-      <section >
-          {/* <div className="row section-head">
+      <section id="contact">
+      
+          <div className="row section-head">
             <div className="ten columns">
               <p className="lead">
               Feel free to contact me for any work or suggestions below
@@ -13,15 +59,46 @@ export default class ContactUs extends Component {
             </div>
           </div>
           <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
-              </div>
-            </aside>
-          </div> */}
+            <div className="six columns">
+                <aside className="eigth columns footer-widgets">
+                  <div className="widget">
+                    <h4>Linked in :
+                      {resumeData.linkedinId}
+                    </h4>
+                  </div>
+                </aside>
+            </div>
+            <div className="six columns">
+              <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                  <label>
+                    Name:
+                    <input type="text" value={this.state.name} onChange={this.handleChangeName} autoComplete={false}/>
+                  </label>
+                </div>
+                <div className="row">
+                  <label>
+                    Email:
+                    <input type="text" value={this.state.email} onChange={this.handleChangeEmail} />
+                  </label>
+                </div>
+                <div className="row">
+                  <label>
+                    Message:
+                    <textarea value={this.state.message} onChange={this.handleChangeMessage} />
+                  </label>
+                </div>  
+                <div className="row submit">
+                  <input type="submit" value="Send" id = "contact_submit"/>
+                </div>         
+                          
+              </form>
+            
+            </div>
+            
+          </div>
         </section>
         );
   }
 }
+
